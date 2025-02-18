baa499e5201438ebbae510a89a7818b05a3b3565
Dan Simonin <simonindan@gmail.com>
2025-02-18 10:17:06 +0100

    [+] Added name to videomessage

--- Chat/VideoMessage.cs
+++ Chat/VideoMessage.cs
@@ -9,10 +9,11 @@ namespace EscapeLifeCommon.Messages.Chat
     public partial class VideoMessage : MessageBase
     {
         public Uri URL { get; set; }
+        public string Name { get; set; }
             
         public override string ToString()
         {
-            return $"{base.ToString()} a {GetType().Name} with URL '{URL}'";
+            return $"{base.ToString()} a {GetType().Name} with URL '{URL}' and Name '{Name}'";
         }
     }
 }
