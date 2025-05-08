2fa9f1a49af461a1ec4232ef533fabfdf789b441
MikleRe <simonindan@gmail.com>
2025-05-08 14:31:05 +0200

    [+] querier in triggerkeymessage

--- TriggerKey/TriggerKeyMessage.cs
+++ TriggerKey/TriggerKeyMessage.cs
@@ -8,6 +8,7 @@
         public TriggerKeyMessageType Type { get; set; }
         public string TriggerKey { get; set; }
         public string StepName { get; set; }
+        public string Querier { get; set; } // the person who tried the trigger key
 
         public override string ToString()
         {
