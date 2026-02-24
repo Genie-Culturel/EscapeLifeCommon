744a4ba5b7bcfabc28e98af18a8cbe6c8800ae48
CristianaLima <92742855+CristianaLima@users.noreply.github.com>
2026-02-24 13:34:47 +0100

    Update AutomaticMessage.cs

--- Chat/AutomaticMessage.cs
+++ Chat/AutomaticMessage.cs
@@ -13,7 +13,7 @@ namespace EscapeLifeCommon.Messages.Chat
         public Dictionary<string, string> LocalizedStrings = new();
 
         // NEW: optional audio per language
-        public Dictionary<string, string> AudioUrls { get; set; }
+        public Dictionary<string, string> AudioUrls { get; set; } = new();
 
         public override string ToString()
         {
